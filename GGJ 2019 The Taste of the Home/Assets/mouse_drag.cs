0e2dcf4899db323b74ca9dec51e55230751a5bb8
李艺安 <yianl2@uci.edu>
2019-01-26 10:42:10 -0800

    fry pan scene

---
+++ GGJ 2019 The Taste of the Home/Assets/mouse_drag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_drag : MonoBehaviour
+{
+    float distance = 10;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = objectPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
