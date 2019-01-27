048b2e906f95f9b34c9ce7009619658c78a004e6
d-guzman <dghost121@gmail.com>
2019-01-26 22:07:24 -0800

    Implemented Player Model and "Animations"
    Player model is a blender model, so to rotate on the Y axis, you gotta
    rotate on its Z axis.
    
    I made the PlayerAnimation script with public functions that will do an
    animation for a preset amount of time.
    
    If you want to have a set face, you have to do that yourself using an
    array of gameobjects which are the faces.

---
+++ GGJ 2019 The Taste of the Home/Assets/Scripts/PlayerRelated/PlayerAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public float timeBlinking = .15f;
+    public float timeExpression = .2f;
+    // 0 = normal, 1 = blinking, 2 = happy
+    // 3 = annoyed, 4 = sad, 5 = hit
+    public GameObject[] playerFaces;
+    private bool goDo = true;
+
+    // Private Methods
+    private IEnumerator doExpression(int i, float runtime)
+    {
+        playerFaces[i].SetActive(true);
+        playerFaces[0].SetActive(false);
+        yield return new WaitForSeconds(runtime);
+        playerFaces[0].SetActive(true);
+        playerFaces[i].SetActive(false);
+    }
+
+    // Public Methods
+    public void doBlinkAnim()
+    {
+        StartCoroutine(doExpression(1, timeBlinking));
+    }
+
+    public void doHappyAnim()
+    {
+        StartCoroutine(doExpression(2, timeExpression));
+    }
+
+    public void doAnnoyedAnim()
+    {
+        StartCoroutine(doExpression(3, timeExpression));
+    }
+
+    public void doSadAnim()
+    {
+        StartCoroutine(doExpression(4, timeExpression));
+    }
+
+    public void doHitAnim()
+    {
+        StartCoroutine(doExpression(5, timeExpression));
+    }
+}
